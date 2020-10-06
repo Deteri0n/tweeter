b7deb66cb3d885027ea509868fa80e22cd95b731
Etienne LC <etlecoent@gmail.com>
2020-10-06 21:35:10 +0000

    Fixed the nav bar and reverted to bolder text on hover

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,6 @@ $(document).ready(() => {
 
   $('#tweet-text').keyup(function() {
     let remainingChars = 140 - $(this).val().length;
-    console.log($(this).siblings('.submit-button').find('.counter'));
     if (remainingChars < 0) {
       $(this).siblings('.submit-button').find('.counter').attr('value',"negative");
     } else {
