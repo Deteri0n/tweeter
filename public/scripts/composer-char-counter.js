f039511000197001022ff62a925bc9d6c0b56e48
Etienne LC <etlecoent@gmail.com>
2020-10-06 19:14:41 +0000

    Change the color of counter depending of the nb of chars in the text box

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,8 +2,13 @@ $(document).ready(() => {
 
   $('#tweet-text').keyup(function() {
     let remainingChars = 140 - $(this).val().length;
-    console.log($(this).siblings())
+    console.log($(this).siblings('.submit-button').find('.counter'));
+    if (remainingChars < 0) {
+      $(this).siblings('.submit-button').find('.counter').attr('value',"negative");
+    } else {
+      $(this).siblings('.submit-button').find('.counter').attr('value', "positive");
+    }
     $(this).siblings('.submit-button').find('.counter').text(remainingChars);
   })
-  
+
 });
