d86617580659523d58024aabee1a201645c699c8
Etienne LC <etlecoent@gmail.com>
2020-10-07 18:22:41 +0000

    Created 2 functions to add tweets to the html from an array

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,68 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(() => {
 
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+ 
+ 
+  const createTweetElement = (tweetObj) => {
+
+    let htmlText =  `<header>
+                      <div class='picture-name'>
+                        <img src=${tweetObj.user.avatars}>
+                        <a>${tweetObj.user.name}</a>
+                      </div>
+                      <div>
+                        <a>${tweetObj.user.handle}</a>
+                      </div>
+                    </header>
+                    <p>${tweetObj.content.text}</p>
+                    <footer>
+                      <time>${Date(tweetObj.created_at)}</time>
+                      <div class='social'>
+                        <i class='fas fa-flag'></i>
+                        <i class='fas fa-retweet'></i>
+                        <i class='fas fa-heart'></i>
+                      </div>
+                    </footer>`;
+    
+    let $tweet = $('<article>').append(htmlText);
+
+    return $tweet;
+  }
+
+  const renderTweets = (tweetsArr) => {
+    tweetsArr.forEach(element => {
+      let $tweet = createTweetElement(element);
+      $('#tweets-container').append($tweet);
+    });
+  }
+
+  renderTweets(data);
+
+})
